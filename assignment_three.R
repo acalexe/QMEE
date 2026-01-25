1c1e23d09414547452701c396ff6475ad2917f64
Andrei Alexe <andreialexe@hotmail.com>
2026-01-25 13:00:26 -0500

    Commit from RStudio.

--- assignment_three.R
+++ assignment_three.R
@@ -25,16 +25,18 @@ dataset_two_normalized <- dataset_two |>
 ### ACA: combining the two datasets into one data frame.
 ### ACA: I used bind_rows(). Not sure if there is a better option.
 
-dataset_all_normalized |> bind_rows(
+dataset_all_normalized <-  bind_rows(
   dataset_one_normalized |> mutate(cell = "cell 1"),
   dataset_two_normalized |> mutate(cell = "cell 2")
 )
 
 ggplot(dataset_all_normalized, aes(synaptic.vesicle.type, proportion, color = synaptic.terminal.id)) +
-  geom_point() +
+  geom_point(size = 2,  position = "jitter") +
   facet_wrap(~ cell) +
   labs(x = "Vesicle Type",
        y = "Proportion per Terminal",
        color = "Terminal ID",
        title = "Proportion of Synaptic Vesciles per Synaptic Terminal in Two Distinct Cells"
-)
+  ) +
+  theme_light() +
+  theme(axis.text.x = element_text(size = 8, angle = 45, hjust = 1))
