dcd1c80820044b1d86dae41b9c331f72b120f48e
Andrei Alexe <andreialexe@hotmail.com>
2026-01-25 12:38:31 -0500

    Commit from RStudio.

--- assignment_three.R
+++ assignment_three.R
@@ -1,42 +1,40 @@
 library(tidyverse)
 library(ggplot2)
-library(patchwork)
+
+### ACA: insert text.
 
 dataset_one <- read.csv("synaptic_terminal_data_one.csv")
 
 dataset_two <- read.csv("synaptic_terminal_data_two.csv")
+
+### ACA: insert text. Normalize vesicle type data within each synaptic terminal.
 
 dataset_one_normalized <- dataset_one |> 
   group_by(synaptic.terminal.id) |> 
   mutate(total_vesicles = sum(count),
          proportion = count / total_vesicles) |> 
   ungroup()
-  
+
 
 dataset_two_normalized <- dataset_two |> 
   group_by(synaptic.terminal.id) |> 
   mutate(total_vesicles = sum(count),
          proportion = count / total_vesicles) |> 
   ungroup()
-  
 
-cell_one <- ggplot(dataset_one_normalized, aes(synaptic.terminal.id, proportion, fill = synaptic.vesicle.type)) +
-  geom_col(position = "dodge") +
-  labs(
-    x = "synaptic terminal id",
-    y = "vesicle type proportion",
-    fill = "vesicle type"
-  ) +
-  theme_classic()
+### ACA: combining the two datasets into one data frame.
+### ACA: I used bind_rows(). Not sure if there is a better option.
 
-cell_two <- ggplot(dataset_two_normalized, aes(synaptic.terminal.id, proportion, fill = synaptic.vesicle.type)) +
-  geom_col(position = "dodge") +
-  labs(
-    x = "synaptic terminal id",
-    y = "vesicle type proportion",
-    fill = "vesicle type"
-  ) +
-  theme_classic()
+dataset_all_normalized |> bind_rows(
+  dataset_one_normalized |> mutate(cell = "cell 1"),
+  dataset_two_normalized |> mutate(cell = "cell 2")
+)
 
-cell_one + cell_two
-         
+ggplot(dataset_all_normalized, aes(synaptic.vesicle.type, proportion, color = synaptic.terminal.id)) +
+  geom_point() +
+  facet_wrap(~ cell) +
+  labs(x = "Vesicle Type",
+       y = "Proportion per Terminal",
+       color = "Terminal ID",
+       title = "Proportion of Synaptic Vesciles per Synaptic Terminal in Two Distinct Cells"
+)
